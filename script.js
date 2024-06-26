a1c71fcfc8eae2d1756f9f6d09f78bb87f50130e
neerajaav02 <neerajaav2@gmail.com>
2024-06-26 18:22:34 +0530

    Data type config

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 console.log("Hello World");
+
+
+var a = 10;
+console.log(a);
+var a = "Name"
+console.log(a);
+
+let name = "john";
+console.log(name);
+
+const pi = 3.14;
+console.log(pi);
+// const pi =3.14
+
+
+
+// nan
+
+let valueone = 2;
+let valuetwo = "a";
+
+console.log(valueone+valuetwo);
+
+console.log(valueone+parseInt(valuetwo));
+
+
+
+// const name = "john";
+const age = 30;
+const rating = 4.5; 
+const isCool = true;
+const x = null;
+var y;
+
+console.log(typeof name);
+
+
+console.log("My name is " + name + " and i am " +age);
+//console.log('My name is ${name} and  i am ${age}');
+
